54451d5ad42461ec9fcb7e94046cdfe9ab3ffea1
Dmytro Savostianov <dmytro.savostianov@gen.tech>
2021-11-06 19:36:39 +0200

    [FIX] Config parsing

--- Scripts/Editor/Tools/Configs/ConfigParser.cs
+++ Scripts/Editor/Tools/Configs/ConfigParser.cs
@@ -1,12 +1,31 @@
 using System;
+using System.IO;
+using GoodMoodGames.Scripts.Runtime.Core.Configs;
+using Newtonsoft.Json;
+using UnityEditor;
+using UnityEngine;
 
 namespace GoodMoodGames.Scripts.Editor.Tools.Configs
 {
     public static class ConfigParser
     {
+        private const string ResourcesFolder = "Resources";
+
         public static Action<string, string> OnTryParseConfig;
 
         public static void TryParseConfig(string configName, string configSpreadSheetId) =>
             OnTryParseConfig?.Invoke(configName, configSpreadSheetId);
+
+        public static void SerializeConfig(object config, string configName)
+        {
+            var configPath = GetConfigLocalPath();
+            var configJson = GetConfigJson();
+
+            File.WriteAllText(configPath, configJson);
+            AssetDatabase.Refresh();
+
+            string GetConfigLocalPath() => Path.Combine(Application.dataPath, ResourcesFolder, configName + ".json");
+            string GetConfigJson() => JsonConvert.SerializeObject(config, ConfigsContainer.ConfigSerializerSettings);
+        }
     }
 }
--- Scripts/Runtime/Core/Configs/ConfigsContainer.cs
+++ Scripts/Runtime/Core/Configs/ConfigsContainer.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace GoodMoodGames.Scripts.Runtime.Core.Configs
 {
     public static class ConfigsContainer
     {
+        public static readonly JsonSerializerSettings ConfigSerializerSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            TypeNameHandling = TypeNameHandling.All
+        };
+        
         private static readonly Dictionary<Type, Config> Configs = new Dictionary<Type, Config>();
 
         public static void SetData(params object[] configContainers)
@@ -23,6 +30,12 @@ namespace GoodMoodGames.Scripts.Runtime.Core.Configs
             Debug.Log($"Parsed configs:\n{string.Join("\n", Configs.Select(x => x.Key))}");
         }
 
+        public static TConfig DeserializeConfig<TConfig>(string configName)
+        {
+            var config = Resources.Load<TextAsset>(configName).text;
+            return JsonConvert.DeserializeObject<TConfig>(config, ConfigSerializerSettings);
+        }
+        
         public static T Get<T>() where T : Config => (T) Get(typeof(T));
 
         private static Config Get(Type configType)
